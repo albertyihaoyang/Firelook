88c5e3ad22cc18f7d27468023fd26d25a51ea1fb
杨以皓 <yihao@yangyihaodeMacBook-Pro.local>
2017-10-09 23:35:20 -0700

    1009

--- app/src/main/java/com/yihao/eventreporter/EventFragment.java
+++ app/src/main/java/com/yihao/eventreporter/EventFragment.java
@@ -5,12 +5,17 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.List;
 
 
 /**
